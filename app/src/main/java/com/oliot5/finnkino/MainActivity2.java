e413441a71f68f74df162591f1bceb438038b715
oivakiiski <oiva.kiiski@gmail.com>
2022-04-14 12:18:35 +0300

    Olio-ohjelmoinnin HT Finnkino

--- app/src/main/java/com/oliot5/finnkino/MainActivity2.java
+++ app/src/main/java/com/oliot5/finnkino/MainActivity2.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,12 +16,30 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Fragment fragment;
+        View.OnClickListener listener = new View.OnClickListener() {
+            Fragment fragment;
 
-        fragment = new Fragment_Main();
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.fragmentwindow,fragment);
-        transaction.commit();
+            @Override
+            public void onClick(View view) {
+                if ( view == findViewById(R.id.MovieManagerButton)){
+                    System.out.println("Moikka");
+                    fragment = new Fragment_Main();
+                } else {
+                    fragment = new Fragment_history();
+                    System.out.println("Help me!");
+                }
+
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentwindow,fragment);
+                transaction.commit();
+            }
+        };
+
+        Button movieManagerButton = findViewById(R.id.MovieManagerButton);
+        movieManagerButton.setOnClickListener(listener);
+        Button historyButton = findViewById(R.id.historyButton);
+        historyButton.setOnClickListener(listener);
+
     }
 }
