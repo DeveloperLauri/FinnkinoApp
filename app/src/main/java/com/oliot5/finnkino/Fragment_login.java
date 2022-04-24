d50981724fb7760650ffc5782dc02076d66785a1
Lauri Inkilä <lauri.j.inkila@gmail.com>
2022-04-24 12:04:02 +0300

    Some bugs fixed related to xml-file initialization. Changing languages functionality implemented.

--- app/src/main/java/com/oliot5/finnkino/Fragment_login.java
+++ app/src/main/java/com/oliot5/finnkino/Fragment_login.java
@@ -2,6 +2,8 @@ package com.oliot5.finnkino;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,6 +17,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import org.w3c.dom.Document;
@@ -25,6 +28,7 @@ import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -70,6 +74,8 @@ public class Fragment_login extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        loadLocale();
+
         usernameEditText = (EditText) this.view.findViewById(R.id.editTextUsernameLogin);
         usernameEditText.addTextChangedListener(textWatcher);
 
@@ -77,6 +83,16 @@ public class Fragment_login extends Fragment {
         passwordEditText.addTextChangedListener(textWatcher);
 
         combinationCheckTextView = (TextView) this.view.findViewById(R.id.combinationCheckWindowLogin);
+
+        //functionality for changing language button
+//        Button changeLang = view.findViewById(R.id.changeLang123);
+//        changeLang.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.println("Toimiiko kielen vaihto nappi?");
+//                showChangeLangDialog();
+//            }
+//        });
 
         buttonLogin = view.findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
@@ -100,14 +116,14 @@ public class Fragment_login extends Fragment {
 
                     NodeList nList = doc.getDocumentElement().getElementsByTagName("User");
 
-                    for(int i = 0; i < nList.getLength(); i++) {
+                    for (int i = 0; i < nList.getLength(); i++) {
                         Node node = nList.item(i);
 
                         if (node.getNodeType() == Node.ELEMENT_NODE) {
                             Element element = (Element) node;
 
-                            if(element.getElementsByTagName("Username").item(0).getTextContent().equals(username) && element.getElementsByTagName("Password").item(0).getTextContent().equals(password)) {
-                                if(help == 1) {
+                            if (element.getElementsByTagName("Username").item(0).getTextContent().equals(username) && element.getElementsByTagName("Password").item(0).getTextContent().equals(password)) {
+                                if (help == 1) {
                                     help = 2;
                                     sendToActivity(username);
                                 }
@@ -118,7 +134,6 @@ public class Fragment_login extends Fragment {
 
                         }
                     }
-
 
 
                 } catch (ParserConfigurationException e) {
@@ -140,3 +155,52 @@ public class Fragment_login extends Fragment {
         startActivity(intent);
     }
 }
+
+//    public void showChangeLangDialog() {
+//        //this array stores the languages to be displayed
+//        Context context_2 = getActivity().getApplicationContext();
+//        final String[] listingOfItems = {"English", "Spanish", "Finnish"};
+//        AlertDialog.Builder lBuilder = new AlertDialog.Builder(context_2);
+//        lBuilder.setTitle("Choose lang...");
+//        lBuilder.setSingleChoiceItems(listingOfItems, -1, (dialogInterface, i) -> {
+//            if (i == 0) {
+//                //English
+//                setLocale("en");
+//                recreate();
+//            } else if (i == 1) {
+//                //Spanish
+//                setLocale("es");
+//                recreate();
+//            } else if (i == 2) {
+//                //Finnish
+//                setLocale("fi");
+//                recreate();
+//            }
+//
+//            //dismissing the dialog when a language gets selected
+//            dialogInterface.dismiss();
+//        });
+//
+//        AlertDialog lDialog = lBuilder.create();
+//        lDialog.show();
+//    }
+
+//    private void setLocale(String lang) {
+//        Context context_2 = getActivity().getApplicationContext();
+//        Locale locale = new Locale(lang);
+//        Locale.setDefault(locale);
+//        Configuration config = new Configuration();
+//        config.locale = locale;
+//        context_2.getResources().updateConfiguration(config, context_2.getResources().getDisplayMetrics());
+//
+//        //saving data to the preferences that are shared
+//        SharedPreferences.Editor edit = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+//        edit.putString("My language", lang);
+//        edit.apply();
+//    }
+//
+//    public void loadLocale(){
+//        SharedPreferences preferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+//        String lang1 = preferences.getString("My lang", "");
+//        setLocale(lang1);
+//    }
--- app/src/main/java/com/oliot5/finnkino/MainActivity.java
+++ app/src/main/java/com/oliot5/finnkino/MainActivity.java
@@ -1,15 +1,22 @@
 package com.oliot5.finnkino;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +24,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        loadLocale();
         setContentView(R.layout.activity_main);
+
+        //changing the title of the actionbar
+        ActionBar aBar = getSupportActionBar();
+        aBar.setTitle(getResources().getString(R.string.app_name));
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        //functionality for changing language button
+        Button changeLang = findViewById(R.id.changeLang123);
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Toimiiko kielen vaihto nappi?");
+                showChangeLangDialog();
+            }
+        });
 
+        //functionality for registrating ang login buttons
         View.OnClickListener listener = new View.OnClickListener() {
             Fragment fragment;
 
@@ -42,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction transaction = manager.beginTransaction();
                 transaction.replace(R.id.fragmentwindow,fragment);
                 transaction.commit();
+
             }
         };
 
@@ -49,7 +72,53 @@ public class MainActivity extends AppCompatActivity {
         buttonToLogin.setOnClickListener(listener);
         Button buttonToRegister = findViewById(R.id.buttonToRegistrate);
         buttonToRegister.setOnClickListener(listener);
+    }
 
+    public void showChangeLangDialog() {
+        //this array stores the languages to be displayed
+        final String[] listingOfItems = {"English", "Spanish", "Finnish"};
+        AlertDialog.Builder lBuilder = new AlertDialog.Builder(MainActivity.this);
+        lBuilder.setTitle("Choose lang...");
+        lBuilder.setSingleChoiceItems(listingOfItems, -1, (dialogInterface, i) -> {
+            if (i == 0) {
+                //English
+                setLocale("en");
+                recreate();
+            } else if (i == 1) {
+                //Spanish
+                setLocale("es");
+                recreate();
+            } else if (i == 2) {
+                //Finnish
+                setLocale("fi");
+                recreate();
+            }
+
+            //dismissing the dialog when a language gets selected
+            dialogInterface.dismiss();
+        });
+
+        AlertDialog lDialog = lBuilder.create();
+        lDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        //saving data to the preferences that are shared
+        SharedPreferences.Editor edit = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        edit.putString("My language", lang);
+        edit.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences preferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String lang1 = preferences.getString("My lang", "");
+        setLocale(lang1);
     }
 
     public void logIn(String un) {
@@ -59,8 +128,6 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 1);
         //startActivity(intent);
     }
-
-
 
     // Method to do info change in between frag and activ
     @Override
@@ -75,8 +142,6 @@ public class MainActivity extends AppCompatActivity {
                 text = "";
                 logIn(text);
             }*/
-
-
 
         } catch (Exception e) {
 
